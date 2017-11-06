4c0e962be61fcc3a768834b60f7989d15fb740f7
lizhenmin <andyhome163@163.com>
2017-11-07 07:59:57 +0800

    增加easyocr 测试

--- src/main/java/com/mljr/ocr/CaptchaOcrDemo.java
+++ src/main/java/com/mljr/ocr/CaptchaOcrDemo.java
@@ -18,6 +18,8 @@ import com.mljr.ocr.util.ClearImageHelper;
 import com.mljr.ocr.util.Config;
 import com.mljr.ocr.util.ImageUtil;
 
+import cn.easyproject.easyocr.EasyOCR;
+import cn.easyproject.easyocr.ImageType;
 import net.sourceforge.tess4j.ITessAPI.TessPageSegMode;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
@@ -33,9 +35,12 @@ public class CaptchaOcrDemo {
 
 
     public static void main(String[] args) throws IOException {
-    	getImage();
+    //	getImage();
 
-    	ocrImage();
+    	ocrImageOne();
+    	//ocrImage();
+    	//easyOCRTest();
+    	
     }
     
     
@@ -121,6 +126,55 @@ public class CaptchaOcrDemo {
 
 
        // aspriseOcr(testImg);
+   }
+   
+   private static void ocrImageOne() throws IOException{
+	   File srcfile = new File(Config.imageTmpPath + "test.jpg");
+	   File filepicF = new File(Config.imageTmpPath + "test0.jpg");
+	   
+	   ClearImageHelper.cleanImage(srcfile, filepicF);
+      
+       Tesseract tessreact = new Tesseract();
+       tessreact.setTessVariable("tessedit_char_whitelist", "0123456789");//设置限定字符
+       tessreact.setPageSegMode(TessPageSegMode.PSM_SINGLE_CHAR);//设定单个字符
+       tessreact.setDatapath(Config.tessdataPath);
+       try {
+
+           // 这里对图片黑白处理,增强识别率.这里先通过截图,截取图片中需要识别的部分
+           //	BufferedImage textImage = ImageHelper.convertImageToGrayscale(ImageHelper.getSubImage(testImg, 0, 0, 100, 30));
+           // 图片锐化,自己使用中影响识别率的主要因素是针式打印机字迹不连贯,所以锐化反而降低识别率
+           // textImage = ImageHelper.convertImageToBinary(textImage);
+           // 图片放大5倍,增强识别率(很多图片本身无法识别,放大5倍时就可以轻易识,但是考滤到客户电脑配置低,针式打印机打印不连贯的问题,这里就放大5倍)
+           //textImage = ImageHelper.getScaledInstance(textImage, endX * 5, endY * 5);
+
+           String result = tessreact.doOCR(srcfile).replaceAll("\\n", "");
+           
+           System.out.println(result);
+       } catch (TesseractException e) {
+           System.err.println(e.getMessage());
+       }
+
+
+       // aspriseOcr(testImg);
+   }
+   
+   static void   easyOCRTest(){
+	  EasyOCR e = new EasyOCR();
+	  e.setTesseractPath("/usr/local/bin/tesseract");
+	 
+//    e.setTesseractOptions("-l apofont");
+    e.setTesseractOptions("-l eng");
+
+//直接识别图片内容
+//    System.out.println(e.discern("images/test6.php.png"));
+//直接识别验证码图片内容
+//    System.out.println(e.discernAndAutoCleanImage("images/22222.png", ImageType.CAPTCHA_INTERFERENCE_LINE));
+//验证码图片，经过：普通清理、形变场景自动一体化处理后，识别内容
+    System.out.println(e.discernAndAutoCleanImage(Config.imageTmpPath + "test.jpg", ImageType.CLEAR));
+    System.out.println(e.discernAndAutoCleanImage(Config.imageTmpPath + "test.jpg", ImageType.CAPTCHA_WHITE_CHAR));
+    System.out.println(e.discernAndAutoCleanImage(Config.imageTmpPath + "test.jpg", ImageType.CAPTCHA_WHITE_CHAR));
+//    System.out.println(e.discernAndAutoCleanImage("images/test6.php.png",ImageType.CAPTCHA_SPOT));
+
    }
     
  /*  private  static void aspriseOcr(File imageFile) {
